15022932e1af3798689a8efcef68b0e7fc192090
Andy Humes <andrew.humes@du.edu>
2022-03-14 12:46:37 -0600

    finished search all

--- server.js
+++ server.js
@@ -7,44 +7,54 @@ const fs = require("fs");
 
 const mysql = require("mysql2");
 
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
 
-const db = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "employees_db",
-    },
-    console.log("Connected to employees_db on Port 3001")
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "employees_db",
+  },
+  console.log("Connected to employees_db on Port 3001")
 );
 
 inquirer
-    .prompt([{
-        type: "list",
-        name: "viewAll",
-        message: "What would you like to view (all)?",
-        choices: ["Department", "Role", "Employee"],
-    }])
+  .prompt([
+    {
+      type: "list",
+      name: "viewAll",
+      message: "What would you like to view (all)?",
+      choices: ["Department", "Role", "Employee"],
+    },
+  ])
 
-    .then((answer) => {
-        switch (answer.viewAll) {
-            case "Department":
-                db.query("SELECT * FROM department", function (err, answer) {
-                    console.table(answer);
-                })
-                break;
-            
-      
-            
-            }}); 
+  .then((answer) => {
+    switch (answer.viewAll) {
+      case "Department":
+        db.query("SELECT * FROM department", function (err, answer) {
+          console.table(answer);
+        });
+        break;
 
-      
+      case "Role":
+        db.query("SELECT * FROM role", function (err, answer) {
+          console.table(answer);
+        });
+        break;
 
-
-            
+      case "Employee":
+        db.query("SELECT * FROM employee", function (err, answer) {
+          console.table(answer);
+        });
+        break;
+    }
+  });
 
 app.listen(PORT, () => console.log("Server started on port: 3001"));
 
